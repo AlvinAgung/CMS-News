fd3f17cc1c705532207c4dd6235b7449efe5b9f6
AlvinAgung <alvinagung170@gmail.com>
2023-12-06 13:56:30 +0700

    12-06-2023

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -14,54 +14,54 @@
     <div class="shadow-bottom"></div>
     <div class="main-menu-content">
         <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
-            <li class=" nav-item"><a class="d-flex align-items-center" href="{{ route('dashboard') }}"><i
+            <li class="nav-item {{ Request::routeIs(['dashboard']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('dashboard') }}"><i
                         data-feather="activity"></i><span class="menu-title text-truncate"
                         data-i18n="Kanban">Dashboard</span></a>
             </li>
-            <li class=" nav-item"><a class="d-flex align-items-center" href="#"><i
+            <li class="nav-item"><a class="d-flex align-items-center" href="#"><i
                         data-feather="file-text"></i><span class="menu-title text-truncate"
                         data-i18n="Invoice">Management</span></a>
                 <ul class="menu-content">
                     @if ( \Auth::user()->role_id == '1' )
                         
-                        <li><a class="d-flex align-items-center" href="{{ route('kategori-berita.index') }}"><i
+                        <li class="{{ Request::routeIs(['kategori-berita.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('kategori-berita.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Kategori</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('berita.index') }}"><i
+                        <li class="{{ Request::routeIs(['berita.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('berita.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Berita</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('acara.index') }}"><i
+                        <li class="{{ Request::routeIs(['acara.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('acara.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Acara</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('video.index') }}"><i
+                        <li class="{{ Request::routeIs(['video.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('video.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Video</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('jabatan.index') }}"><i
+                        <li class="{{ Request::routeIs(['jabatan.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('jabatan.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Jabatan</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('perangkat-desa.index') }}"><i
+                        <li class="{{ Request::routeIs(['perangkat-desa.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('perangkat-desa.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Perangkat Desa</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('role.index') }}"><i
+                        <li class="{{ Request::routeIs(['role.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('role.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="Preview">Role</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('user.index') }}"><i
+                        <li class="{{ Request::routeIs(['user.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('user.index') }}"><i
                                     data-feather="circle"></i><span class="menu-item text-truncate"
                                     data-i18n="Preview">User</span></a>
                         </li>
                     @elseif(\Auth::user()->role_id == '2' )
-                        <li><a class="d-flex align-items-center" href="{{ route('acara.index') }}"><i
+                        <li class="{{ Request::routeIs(['acara.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('acara.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Acara</span></a>
                         </li>
-                        <li><a class="d-flex align-items-center" href="{{ route('berita.index') }}"><i
+                        <li class="{{ Request::routeIs(['berita.index']) ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('berita.index') }}"><i
                             data-feather="circle"></i><span class="menu-item text-truncate"
                             data-i18n="List">Berita</span></a>
                         </li>
